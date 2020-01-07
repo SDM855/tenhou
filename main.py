67e80d6084ff7e7d362df163df739a0a56205f66
SDM855 <47089983+SDM855@users.noreply.github.com>
2020-01-07 18:01:24 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from http.server import HTTPServer,BaseHTTPRequestHandler
+import json
+
+data = {'result':'this is a test'}
+host = ('localhost', 12121)
+
+
+class Request(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode())
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        data = json.loads(post_data)
+        print(data)
+
+
+
+if __name__ == '__main__':
+    server = HTTPServer(host, Request)
+    print('Starting server, listen at: %s:%s' % host)
+    server.serve_forever()
